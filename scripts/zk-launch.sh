68f3dd1abc112950f5be9871a1b4449dd4c2a1f1
Eddie S. Yim <eddie2yim@gmail.com>
2022-08-24 11:43:45 +0900

    Update zk-launch.sh

--- scripts/zk-launch.sh
+++ scripts/zk-launch.sh
@@ -7,4 +7,9 @@ echo '>> Install corretto Java 17' && \
 sudo yum install -y https://corretto.aws/downloads/latest/amazon-corretto-17-aarch64-linux-jdk.rpm && \
 javainstalled=$(readlink -f $(which java)) && \
 sudo echo "export JAVA_HOME=${javainstalled/\/bin\/java/}" >> /etc/profile && \
-sudo source /etc/profile;
+sudo source /etc/profile && \
+echo '>> Create group and user named zookeeper' && \
+sudo groupadd zookeeper && \
+sudo useradd -g zookeeper zookeeper && \
+sudo mkdir -p /var/lib/zookeeper/data && \
+mkdir -p /var/lib/zookeeper/logs ;
