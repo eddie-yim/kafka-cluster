89299a2002fa27936f495893e4760c5b3aeb9c22
Eddie S. Yim <eddie2yim@gmail.com>
2022-08-24 19:07:16 +0900

    Create test.sh

---
+++ scripts/test.sh
@@ -0,0 +1,9 @@
+echo -e "export ZK_HOSTS=localhost
+export ZK_MYID=1
+export DLM_ADMIN_PASSWORD=apassword
+export DLM_ZOOKEEPER_PASSWORD=zpassword
+export BROKER_ID=1
+export ZOOKEEPER_CONNECT=localhost:2181
+export SCRAM_ADMIN_PASSWORD=apassword
+export SCRAM_BROKER_PASSWORD=bpassword
+export SCRAM_CLIENT_PASSWORD" >> /etc/profile && source /etc/profile
