23905202c0dc9fb7a0df3a23928a6061afbe8bd1
Eddie S. Yim <eddie2yim@gmail.com>
2022-08-24 11:41:37 +0900

    Update zk-launch.sh

--- scripts/zk-launch.sh
+++ scripts/zk-launch.sh
@@ -6,5 +6,5 @@ sudo yum install -y wget which git vim && \
 echo '>> Install corretto Java 17' && \
 sudo yum install -y https://corretto.aws/downloads/latest/amazon-corretto-17-aarch64-linux-jdk.rpm && \
 javainstalled=$(readlink -f $(which java)) && \
-echo "export JAVA_HOME=${javainstalled/\/bin\/java/}" >> /etc/profile && \
-source /etc/profile;
+sudo echo "export JAVA_HOME=${javainstalled/\/bin\/java/}" >> /etc/profile && \
+sudo source /etc/profile;
