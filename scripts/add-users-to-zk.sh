013dd6c8fcb8f124d989c00a1e0e635c9d03c16d
Eddie S. Yim <eddie2yim@gmail.com>
2022-08-25 13:47:07 +0900

    Update add-users-to-zk.sh

--- scripts/add-users-to-zk.sh
+++ scripts/add-users-to-zk.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
-echo -e "\n### INSTALL KAFKA ###"
-if [ ! -e /usr/local/kafka_2.13-3.2.1/bin ]
-then
-    echo ">> Installing kafka ..."
+# install kafka
+if [ ! -e /usr/local/kafka_2.13-3.2.1/bin ]; then
     sudo wget -P /opt https://archive.apache.org/dist/kafka/3.2.1/kafka_2.13-3.2.1.tgz
     sudo chmod 600 /opt/kafka_2.13-3.2.1.tgz
     sudo tar zxvf /opt/kafka_2.13-3.2.1.tgz -C /usr/local
     sudo ln -s /usr/local/kafka_2.13-3.2.1 /usr/local/kafka
 fi
 
-echo -e "\n### SET KAFKA CONFIG INTO server.properties ###"
-if [ ! -e /var/lib/kafka/data ]
-then
-    sudo mkdir -p /var/lib/kafka/data
-fi
-
-echo -e "\n### REGISTER KAFKA ENITY USERS ###"
+# register kafka entry users
 sh /usr/local/kafka/bin/kafka-configs.sh --zookeeper localhost:2181 --alter --add-config "SCRAM-SHA-512=[password=${SCRAM_CLIENT_PASSWORD}]" --entity-type users --entity-name client
 sh /usr/local/kafka/bin/kafka-configs.sh --zookeeper localhost:2181 --alter --add-config "SCRAM-SHA-512=[password=${SCRAM_BROKER_PASSWORD}]" --entity-type users --entity-name broker
 
-echo -e "\n### RELOAD ZOOKEEPER SYSTEMD ###"
+# reaload zookeeper daemon
 sudo systemctl daemon-reload
