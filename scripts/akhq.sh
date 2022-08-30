119814807d8b6c02d6e53c4fabd4f168343295c9
Eddie S. Yim <eddie2yim@gmail.com>
2022-08-30 11:40:02 +0900

    Update akhq.sh

--- scripts/akhq.sh
+++ scripts/akhq.sh
@@ -9,8 +9,6 @@ echo "
 micronaut:
   server:
     port: 8080
-  security:
-    enabled: true
 akhq:
   connections:
     dev:
@@ -19,12 +17,6 @@ akhq:
         security.protocol: SASL_PLAINTEXT
         sasl.mechanism: SCRAM-SHA-512
         sasl.jaas.config: org.apache.kafka.common.security.scram.ScramLoginModule required username=\"client\" password=\"${SCRAM_CLIENT_PASSWORD}\";
-  security:
-    basic-auth:
-      - username: admin
-        password: \"8D0AB724A7B65D8E1F823473A0349710F4B4015227F51E163F4634D0468DAD33\"
-        groups:
-        - admin
 " > /usr/local/akhq/application.yml
 
 nohup java -Dmicronaut.config.files=/usr/local/akhq/application.yml -jar /usr/local/akhq/akhq-0.21.0-all.jar 1> /dev/null 2>&1 &
